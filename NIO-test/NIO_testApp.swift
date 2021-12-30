587ae10981a4ca31300b279aeaaaa64fb66e8974
Alessio Nossa <alessio.nossa@gmail.com>
2021-12-30 23:42:15 +0100

    Initial Commit

---
+++ NIO-test/NIO_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIO_testApp.swift
+//  NIO-test
+//
+//  Created by Alessio Nossa on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NIO_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
